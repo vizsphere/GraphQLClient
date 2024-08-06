a2cbdb67765161065a63ce9c7b512b29aaa51672
Pervaiz Akthar <hello@vizzon.co.uk>
2024-08-06 22:17:43 +0100

    added mutation to add new speaker

--- graphql-client/src/App.js
+++ graphql-client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useMutation } from '@apollo/client';
 
 const gqlGetSpeakers = gql`
   query getSpeakers {
@@ -12,9 +12,24 @@ const gqlGetSpeakers = gql`
     }
   }
 `
+
+const ADD_SPEAKER = gql`
+  mutation AddSpeaker ($name: String!, $bio: String!, $webSite : String!) {
+  addSpeaker(input: {
+    name: $name,
+    bio: $bio,
+    webSite: $webSite }) {
+    speaker {
+      id
+    }
+  }
+}
+`;
+
 function App() {
   return (
     <div className="App">
+      <AddSpeaker />
       <DisplaySpeakers />
     </div>
   );
@@ -37,4 +52,57 @@ function DisplaySpeakers() {
     </div>
   ));
 }
+
+function AddSpeaker() {
+  let input;
+  const [addNewSpeaker, { data, loading, error }] = useMutation(ADD_SPEAKER);
+
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error}`;
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          addNewSpeaker({ variables: { name: input.value, bio : input.value, webSite : input.value} });
+          input.value = '';
+        }}
+      >
+        <div>
+          <p>Add new speaker</p>
+          <input
+           ref={node => {
+            input = node;
+          }}
+            type="text"
+            placeholder="Enter speaker name"
+          />
+          <br />
+          <br />
+          <input
+           ref={node => {
+            input = node;
+          }}
+            type="text"
+            placeholder="Enter speaker bio"
+          />
+          <br />
+          <br />
+          <input
+            ref={node => {
+              input = node;
+            }}
+            type="text"
+            placeholder="Enter speaker website"
+          />
+          <br />
+          <br />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
 export default App;
--- graphql-client/src/index.js
+++ graphql-client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
 const client = new ApolloClient({
   uri: 'https://localhost:44318/graphql/', /* Clone the repository and run the server locally.  https://github.com/vizzontech/GraphQLServer/  */
   cache: new InMemoryCache()
